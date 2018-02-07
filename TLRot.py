cf823d533b279848428f5e01853f64600e936660
cdbaird <bairdc13@gmail.com>
2018-02-07 10:29:32 +0000

    Add TLRot.py and command list

---
+++ TLRot.py
@@ -0,0 +1,76 @@
+import serial as s
+import serial.tools.list_ports as lp
+import sys
+from cmd import cmd
+
+def find_ports():
+	if sys.platform.startswith('darwin'):
+		ports = [comport.device for comport in lp.comports()]
+		# print(ports)
+	else:
+		raise OSError('Not Implemented for this OS: %s' % sys.platform)
+	
+	avail_ports = []	
+	for port in ports:
+		try:
+			p = s.Serial(port)
+			p.close()
+			avail_ports.append(port)
+		except (OSError, s.SerialException):
+			pass
+
+	return avail_ports
+
+class TLMotor():
+
+	def __init__(self, port, baud=9600, bytesize=8, parity='N'):
+		try:
+			self.motor = s.Serial(port, baud, bytesize, parity)
+
+		except s.SerialException:
+			print('Could not open port %s' % port)
+
+		if self.motor.is_open:
+			self.get_motor_info()
+
+
+	def get_motor_info(self):
+		# instruction = cmd['info']
+		self.info = self.send_command(cmd['info'])
+
+	def home(self, data=b'0'): # 0 for CW, 1 for ACW
+		# instruction = b'ho'		 
+		self.status = self.send_command(cmd['home'], data)
+
+	def fw_jog(self):
+		self.status = self.send_command(cmd['forward'])
+
+	def send_command(self, instruction, data=None, address=b'0'):
+		command = address + instruction
+		if data:
+			command += data
+		#print(command)
+		self.motor.write(command)
+		response = self.motor.read_until(terminator=b'\n')
+		return response
+
+
+def response_parse(response):
+	pass
+		 
+
+
+
+def main():
+	ports = find_ports()
+	mot1 = TLMotor(ports[1])
+	print(mot1.info)
+	mot1.home()
+	#mot1.fw_jog()
+	print(mot1.status)
+
+
+
+
+if __name__ =='__main__':
+	main()
---
+++ cmd.py
@@ -0,0 +1,10 @@
+
+cmd = {'info' : b'in',
+	'status' : b'gs',
+	'home' : b'ho',
+	'forward' : b'fw',
+	'backward' : b'bw'}
+
+if __name__ == '__main__':
+	keys = cmd.keys()
+	print('\n'.join(keys))
