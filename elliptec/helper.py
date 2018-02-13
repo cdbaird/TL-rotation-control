35d78df9097f6331a1eec6b4bd019a77a99cae7e
cdbaird <bairdc13@gmail.com>
2018-02-13 21:43:11 +0000

    Add tapedrive submodule

--- elliptec/helper.py
+++ elliptec/helper.py
@@ -1,7 +1,6 @@
 import serial as s
 import serial.tools.list_ports as lp
 import sys
-
 from .errcodes import error_codes
 
 
--- elliptec/tapedrive/tapedrive.py
+++ elliptec/tapedrive/tapedrive.py
@@ -1,8 +1,34 @@
-import rotation
+import threading
+import os
+import sys
+from configparser import ConfigParser
+from rotation import Motor
 
-def func():
-	print('this is from tapedrive.py')
+class Tapedrive():
+	def __init__(self, motor1=None, motor2=None, cfg='config.ini'):
+		if (motor1 is None) or (motor2 is None):
+			try:
+				self.get_config(cfg)
+				self.connect_motors()
+			except FileNotFoundError:
+				print('No configuration file found!')
 
-def get_ports():
-	print(rotation.find_ports())
+		else:
+			assert isinstance(motor1, elliptec.Motor)
+			assert isinstance(motor2, elliptec.Motor)
+	
+	def get_config(self, cfg):
+		if not os.path.isfile(cfg):
+			raise FileNotFoundError
+		self.config = ConfigParser()
+		self.config.read(cfg)
+
+	def connect_motors(self):
+
+
+
+
+
+		
+
 
